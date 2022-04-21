a6e2dccd099ef9fa5ad74af891a1310f8fe45dc9
anonymous <davidodo2015@gmail.com>
2022-04-21 15:22:47 +0200

    Creating template to display all expenses and dispalying all expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+app_name = 'expenses'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('pending_expense/', views.pending_expenses, name='pending_expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -6,8 +6,17 @@ from .models import Expense
 
 @login_required(login_url='login')
 def home(request):
+    page_name = 'Dashboard'
     profile = request.user.profile
     wallet = profile.wallet
     last_transaction = wallet.owner.topuptransaction_set.last()
-    context = {'wallet': wallet, 'last_transaction': last_transaction}
+    context = {'wallet': wallet, 'page_name': page_name, 'last_transaction': last_transaction}
     return render(request, 'expenses/index.html', context)
+
+
+def pending_expenses(request):
+    page_name = 'Pending Expenses'
+    profile = request.user.profile
+    expenses = profile.todoexpense_set.all()
+    context = {'page_name': page_name, 'pending_expenses': expenses}
+    return render(request, 'expenses/pending_expenses_list.html', context)
