da0f6090fb603704babca161f7b340ec9d7d27b7
anonymous <davidodo2015@gmail.com>
2022-05-19 07:27:19 +0200

    Designing The pending Expense Page, To keep track of all pending expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 app_name = 'expenses'
 urlpatterns = [
-    path('', views.tester, name='home'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    # path('home/', views.home, name='home'),
     path('pending_expense/', views.pending_expenses, name='pending_expense'),
 ]
